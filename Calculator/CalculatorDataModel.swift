e25ed59d1d4f07014bc2eff6fe3e1c4a85c0b3db
Macintosh <macintosh@macintoshs-mac-mini.local>
2016-03-26 20:47:10 -0400

    initial version

---
+++ Calculator/CalculatorDataModel.swift
@@ -0,0 +1,63 @@
+//
+//  CalculatorDataModel.swift
+//  Calculator
+//
+//  Created by Macintosh on 3/26/16.
+//  Copyright © 2016 Macintosh. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorDataModel {
+
+private var firstNumber = 0.0
+private var secondNumber = 0.0
+private var result = 0.0
+private var operation = ""
+
+    func reset() -> String{
+        firstNumber = 0.0
+        secondNumber = 0.0
+        result = 0.0
+        operation = ""
+        return String(result)
+    }
+    
+    func setOperationAndNumber(let operation: String, let secondNum: String){
+        self.operation = operation
+        firstNumber = secondNumber
+        secondNumber = Double(secondNum)!
+        print("first Number =\(firstNumber) operator =\(operation) second number = \(secondNumber)")
+    }
+    
+    func getResult () -> String {
+        return String(result)
+    }
+    
+    func calculate(let secondNum: String) ->String{
+        firstNumber = secondNumber
+        secondNumber = Double(secondNum)!
+        switch operation {
+        case "+":
+            result = firstNumber + secondNumber
+        case "-":
+            result = firstNumber - secondNumber
+        case "*":
+            result = firstNumber * secondNumber
+        case "/":
+            result = firstNumber / secondNumber
+        case "+":
+            result = firstNumber + secondNumber
+        default:
+            print("no operation selected")
+            result = secondNumber
+        }
+//      print(result)
+        return String(result)
+    
+    }
+
+
+    
+
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    var calculator = CalculatorDataModel()
+    var calculationComplete = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +24,52 @@ class ViewController: UIViewController {
     }
 
 
+    
+    @IBOutlet weak var displayLable: UILabel!
+    
+    @IBAction func buttonPressed(sender: UIButton) {
+//      print("button pressed \(sender.titleLabel!.text!)")
+        if(calculationComplete) {
+            calculator.reset();
+            displayLable.text = calculator.getResult()
+            calculationComplete = false
+        }
+        if(displayLable.text! == "0.0") {
+            displayLable.text = sender.titleLabel!.text!
+        } else {
+            displayLable.text = displayLable.text! + sender.titleLabel!.text!
+        }
+     }
+
+    /* New operation could be / * - + = which will prompt us to
+    calculate and display
+    Either this could be the operator, or this could be instruction to calculate.
+
+    */
+    @IBAction func operatorSelected(sender: UIButton) {
+        calculator.setOperationAndNumber(sender.titleLabel!.text!, secondNum: displayLable.text!)
+        displayLable.text = calculator.getResult()
+        
+    }
+    
+    @IBAction func calculate(sender: AnyObject) {
+        displayLable.text = calculator.calculate(displayLable.text!)
+        calculationComplete = true
+    }
+    
+    
+    @IBAction func clear(sender: AnyObject) {
+                displayLable.text = calculator.reset()
+    }
+    
+    @IBAction func flipSign(sender: UIButton) {
+
+        if(displayLable.text!.hasPrefix("-")){
+            displayLable.text!.removeAtIndex(displayLable.text!.startIndex)
+        } else {
+            displayLable.text = "-" + displayLable.text!
+        }
+    }
+    
 }
 
